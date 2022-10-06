e03a1aa1c0bb8ca5aba26d0348940f5cb7e4b484
Paulo Vicente <vcente82@gmail.com>
2022-10-05 22:07:51 -0400

    ...

--- src/layouts/styles.ts
+++ src/layouts/styles.ts
@@ -5,7 +5,6 @@ export const LayoutContainer = styled.div`
   margin: 0 auto;
   background: ${(props) => props.theme.background};
   padding: 1rem;
-  /* border-radius: 8px; */
   display: flex;
   flex-direction: column;
 `
